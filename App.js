a645a03c8de7405cb7cf70c97f1a85388ddeadc1
George Chan Modini <george.c.modini@usace.army.mil>
2022-03-02 22:00:06 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/navigation';
+import Login from './components/login'
+function App() {
+  return (
+    <div className="App">
+     <Navigation /> 
+     <Login /> 
+    </div>
+  );
+}
+
+export default App;
